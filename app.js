d44387198c1a4d3b6df3a44885951bc9f3b6e1bf
mikaelaliwanag <mikaela301@gmail.com>
2020-07-09 19:30:29 -0700

    Create POST route

--- app.js
+++ app.js
@@ -30,10 +30,23 @@ app.get("/articles", function(req, res) {
         }
         
     });
-
 });
 
+app.post("/articles", function(req, res) {
 
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+
+    newArticle.save(function(err) {
+        if(!err){
+            res.send("Successfully added a new article.");
+        } else {
+            res.send(err);
+        }
+    });
+});
 
 
 
