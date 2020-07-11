a0f9c5a485ba4c6a29a80a370f0a75f69e4902eb
mikaelaliwanag <mikaela301@gmail.com>
2020-07-10 23:16:04 -0700

    Refactor routes into a chained route

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function(req, res) {
+app.route("/articles")
+.get(function(req, res) {
 
     Article.find(function(err, foundArticles) {
         if(!err) {
@@ -30,9 +31,9 @@ app.get("/articles", function(req, res) {
         }
         
     });
-});
+})
 
-app.post("/articles", function(req, res) {
+.post(function(req, res) {
 
     const newArticle = new Article({
         title: req.body.title,
@@ -46,9 +47,9 @@ app.post("/articles", function(req, res) {
             res.send(err);
         }
     });
-});
+})
 
-app.delete("/articles", function(req, res) {
+.delete(function(req, res) {
     Article.deleteMany(function(err){
         if(!err) {
             res.send("Successfully deleted all articles.")
@@ -57,6 +58,7 @@ app.delete("/articles", function(req, res) {
         }
     });
 });
+
 
 
 
