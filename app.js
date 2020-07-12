e3fdf155d2816a70ea5e04f64d56350bac2d61e6
mikaelaliwanag <mikaela301@gmail.com>
2020-07-11 21:43:11 -0700

    Create PATCH method

--- app.js
+++ app.js
@@ -87,6 +87,22 @@ app.route("/articles/:articleTitle")
             }
         } 
     );
+})
+
+.patch(function(req, res) {
+
+    Article.update(
+        {title: req.params.articleTitle},
+        {$set: req.body},
+        function(err) {
+            if (!err) {
+                res.send("Successfully updated article.");
+            } else {
+                res.send(err);
+            }
+        }
+    );
+
 });
 
 
